4f656117b1d9f35474b161254cb37ef98bd6383a
Marc <marcvillamongomez@gmail.com>
2026-01-26 08:10:29 +0100

    simplificado el código del ciclo básico añadiendo una string con el resultado sin necesidad de llamar a una función

--- ciclo_basico.py
+++ ciclo_basico.py
@@ -3,8 +3,7 @@ from typing import Any
 
 Cliente = ClienteRefprop(r"C:\Program Files (x86)\REFPROP\REFPRP64.DLL")
 
-
-def calcular_ciclo(fluido: str | list[str], mezcla: list[float],
+def calcular_ciclo_basico(fluido: str | list[str], mezcla: list[float],
                    temperaturas_agua: dict[str, list[float]]) -> dict[str, Any]:
     
 
@@ -77,49 +76,39 @@ def calcular_ciclo(fluido: str | list[str], mezcla: list[float],
         P_water_pinch = TPoint("ETHYLENEGLYCOL", P = 1, H = h_water_pinch)
         pinch = Pk_vap_sat.T - P_water_pinch.T
 
+        # Glide
+        glide_k = Pk_vap_sat.T - Pk_liq_sat.T
+        glide_0 = P0_vap_sat.T - P4.T
 
         puntos = [P1, P2, P3, P4]
+        
+
+        string_resultado = f"""COP = {COP:.4f}
+VCC = {VCC:.4f} kJ/m^3
+Ratio de caudales másicos respecto a refrigerante: Caliente: {ratio_m_GlycolHot_R:.3f}, Frío: {ratio_m_GlycolCold_R:.3f}
+Ratio de caudales volumétricos respecto a refrigerante: Caliente: {ratio_v_GlycolHot_R:.3f}, Frío: {ratio_v_GlycolCold_R:.3f}
+Pinch = {pinch:.2f}ºC
+Temperaturas de condensación: {Pk_vap_sat.T:.1f}ºC y {Pk_liq_sat.T:.1f}ºC: glide = {glide_k:.1f}ºC
+Temperaturas de evaporación: {P0_vap_sat.T:.1f}ºC y {P0_liq_sat.T:.1f}ºC: glide = {glide_0:.1f}ºC
+"""
+        for i, p in enumerate(puntos, start=1):
+            string_resultado += f"Punto {i}: P = {p.P:.2f} bar, H = {p.H:.1f} kJ/kg, T = {p.T:.2f} ºC, Q = {p.Q:.3f}\n"
+        
+
         resultado = {
             "COP": COP,
             "VCC": VCC,
             "puntos": puntos,
             "puntos saturados": puntos_saturados,
-            "Presiones": [PK, P0],
-            "Caudales másicos": [ratio_m_GlycolHot_R, ratio_m_GlycolCold_R],
-            "Caudales volumétricos": [ratio_v_GlycolHot_R, ratio_v_GlycolCold_R],
-            "Pinch": pinch,
+            "presiones": [PK, P0],
+            "caudales másicos": [ratio_m_GlycolHot_R, ratio_m_GlycolCold_R],
+            "caudales volumétricos": [ratio_v_GlycolHot_R, ratio_v_GlycolCold_R],
+            "pinch": pinch,
+            "glide": [glide_k, glide_0],
+            "string resultado": string_resultado,
         }
 
         return resultado
-
-def mostrar_resultados(resultado: dict[str, Any]) -> None:
-    # Extraer datos
-    puntos: list[TPoint] = resultado["puntos"]
-    [P1, P2, P3, P4] = puntos
-    puntos_saturados: list[TPoint] = resultado["puntos saturados"]
-    [Pk_liq_sat, Pk_vap_sat, P0_liq_sat, P0_vap_sat] = puntos_saturados
-    COP = resultado["COP"]
-    VCC = resultado["VCC"]
-    [PK, P0] = resultado["Presiones"]
-    [ratio_m_GlycolHot_R, ratio_m_GlycolCold_R] = resultado["Caudales másicos"]
-    [ratio_v_GlycolHot_R, ratio_v_GlycolCold_R] = resultado["Caudales volumétricos"]
-    pinch = resultado["Pinch"]
-
-    # Representar resultado
-    print(f"COP = {COP:.4f}")
-    print(f"VCC = {VCC:.4f} kJ/m^3")
-
-    print(f"Ratio de caudales másicos respecto a refrigerante: Caliente: {ratio_m_GlycolHot_R:.3f}, Frío: {ratio_m_GlycolCold_R:.3f}")
-    print(f"Ratio de caudales volumétricos respecto a refrigerante: Caliente: {ratio_v_GlycolHot_R:.3f}, Frío: {ratio_v_GlycolCold_R:.3f}")
-    print(f"Pinch = {pinch:.2f}ºC")
-    glide_k = Pk_vap_sat.T - Pk_liq_sat.T
-    glide_0 = P0_vap_sat.T - P4.T
-    print(f"Temperaturas de condensación: {Pk_vap_sat.T:.1f}ºC y {Pk_liq_sat.T:.1f}ºC: glide = {glide_k:.1f}ºC")
-    print(f"Temperaturas de evaporación: {P0_vap_sat.T:.1f}ºC y {P0_liq_sat.T:.1f}ºC: glide = {glide_0:.1f}ºC")
-
-    for i, p in enumerate(puntos, start = 1):
-        print(f"Punto {i}: P = {p.P:.2f} bar, H = {p.H:.1f} kJ/kg, T = {p.T:.2f} ºC, Q = {p.Q:.3f}")
-
 
 # Datos iniciales
 fluido = "PROPANE"
@@ -136,10 +125,10 @@ temperaturas_agua = {
     "t_cw": [t_cw_in, t_cw_out]
 }
 
-resultado = calcular_ciclo(fluido, mezcla, temperaturas_agua)
+resultado = calcular_ciclo_basico(fluido, mezcla, temperaturas_agua)
 
 if __name__ == "__main__":
-    mostrar_resultados(resultado)
+    print(resultado["string resultado"])
     puntos_PH(resultado["puntos"], base_log = 1.5, margen = 0.2)
 
 
